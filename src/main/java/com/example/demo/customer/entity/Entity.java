56f037ce498011946ecec2dccb77ddcc37f7250e
RamtinZare <106357225+RamtinZare@users.noreply.github.com>
2024-04-03 16:26:01 +0330

    Update Entity.java
    enter entity

--- src/main/java/com/example/demo/customer/entity/Entity.java
+++ src/main/java/com/example/demo/customer/entity/Entity.java
@@ -1,4 +1,9 @@
 package com.example.demo.customer.entity;
 
 public class Entity {
+  private String id;
+  private String firstName;
+  private String lastName;
+  private String age;
+  private String Phone;
 }
